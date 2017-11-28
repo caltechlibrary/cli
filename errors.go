fa19576561a5c394ea9b37cb71ef5ba00d504f7a
R. S. Doiel <rsdoiel@gmail.com>
2017-11-27 20:13:57 -0800

    added OnErrror()

--- errors.go
+++ errors.go
@@ -5,6 +5,14 @@ import (
 	"os"
 )
 
+// OnError writes an error message to out if err != nil
+// taking into consideration the state of quiet
+func OnError(out *os.File, err error, quiet bool) {
+	if quiet == false {
+		fmt.Fprint(out, "%s\n", err)
+	}
+}
+
 // ExitOnError is used by the cli programs to
 // handle exit cuasing errors constitantly.
 // E.g. it respects the -quiet flag past to it.
