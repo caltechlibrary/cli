ccb65bc950fd05f84f5b076747ed99dbe0a1ef14
R. S. Doiel <rsdoiel@gmail.com>
2017-11-27 18:51:18 -0800

    Added ExitOnError() for consistant exit error handling in cli

--- errors.go
+++ errors.go
@@ -8,7 +8,7 @@ import (
 // ExitOnError is used by the cli programs to
 // handle exit cuasing errors constitantly.
 // E.g. it respects the -quiet flag past to it.
-func ExitError(out *os.File, err error, quiet bool) {
+func ExitOnError(out *os.File, err error, quiet bool) {
 	if err != nil {
 		if quiet == false {
 			fmt.Fprint(out, "%s\n", err)
